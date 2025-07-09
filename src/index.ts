7834abfce04e22fe96c9ba464c3616aec5474f5d
Evans-thinkpad <13007570214@163.com>
2025-07-09 13:36:01 +0800

    index入口补充组件导出

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,6 @@ export { default as PubForm } from "./lib/PubForm";
 export { default as PubSearch } from "./lib/PubSearch";
 export { default as PubTable } from "./lib/PubTable";
 export { default as PubTitle } from "./lib/PubTitle";
+export { default as PubStepForm } from "./lib/PubStepForm";
+export { default as PubLayout } from "./lib/PubLayout";
+export { default as PubLoginForm } from "./lib/PubLoginForm";
